65ee0e382b14ff507f3f7dd547e76735dc80832b
Fredrick Tieku Avornyo <fredricktieku2@gmail.com>
2025-07-14 02:48:20 +0000

    Completed Book model implementation with CRUD operations

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
 from .models import Book
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')
+    list_display = ('title', 'author', 'publication_year', 'created_at')
+    list_filter = ('author', 'publication_year')
     search_fields = ('title', 'author')
+    ordering = ('-created_at',)
+    date_hierarchy = 'created_at'
+    
+    fieldsets = (
+        ('Basic Information', {
+            'fields': ('title', 'author')
+        }),
+        ('Publication Details', {
+            'fields': ('publication_year',),
+            'classes': ('collapse',)
+        }),
+    )
+
