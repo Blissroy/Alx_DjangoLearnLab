f672c9288297679ef92741bbd0532119c2807717
Blissroy <fredricktieku2@gmail.com>
2025-07-19 18:47:09 +0000

    Update query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,26 +1,28 @@
-from django.db.models import Q
 from relationship_app.models import Author, Book, Library, Librarian
 
 def get_books_by_author(author_name):
-    """
-    Query all books by a specific author using objects.filter()
-    Returns a QuerySet of Book objects
-    """
-    return Book.objects.filter(author__name=author_name)
+    """Query all books by a specific author"""
+    try:
+        author = Author.objects.get(name=author_name)
+        # Fixed: Using objects.filter() as required
+        return Book.objects.filter(author=author)
+    except Author.DoesNotExist:
+        return Book.objects.none()
 
 def get_books_in_library(library_name):
-    """
-    List all books in a library
-    Returns a QuerySet of Book objects
-    """
-    return Book.objects.filter(libraries__name=library_name)
+    """List all books in a library"""
+    try:
+        library = Library.objects.get(name=library_name)
+        # Access books through ManyToMany relationship
+        return library.books.all()
+    except Library.DoesNotExist:
+        return Book.objects.none()
 
 def get_librarian_for_library(library_name):
-    """
-    Retrieve the librarian for a library
-    Returns a Librarian object or None
-    """
+    """Retrieve the librarian for a library"""
     try:
-        return Librarian.objects.get(library__name=library_name)
-    except Librarian.DoesNotExist:
+        library = Library.objects.get(name=library_name)
+        # Access through OneToOne relationship
+        return library.librarian
+    except Library.DoesNotExist:
         return None
