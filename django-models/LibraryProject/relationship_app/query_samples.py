e9318201d5ad011e2e54126b56ba22faec9f6873
Blissroy <fredricktieku2@gmail.com>
2025-07-19 18:45:36 +0000

    Update query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,25 +1,26 @@
+from django.db.models import Q
 from relationship_app.models import Author, Book, Library, Librarian
 
 def get_books_by_author(author_name):
-    """Query all books by a specific author"""
-    try:
-        author = Author.objects.get(name=author_name)
-        return Book.objects.filter(author=author)
-    except Author.DoesNotExist:
-        return []
+    """
+    Query all books by a specific author using objects.filter()
+    Returns a QuerySet of Book objects
+    """
+    return Book.objects.filter(author__name=author_name)
 
 def get_books_in_library(library_name):
-    """List all books in a library"""
-    try:
-        library = Library.objects.get(name=library_name)
-        return library.books.all()
-    except Library.DoesNotExist:
-        return []
+    """
+    List all books in a library
+    Returns a QuerySet of Book objects
+    """
+    return Book.objects.filter(libraries__name=library_name)
 
 def get_librarian_for_library(library_name):
-    """Retrieve the librarian for a library"""
+    """
+    Retrieve the librarian for a library
+    Returns a Librarian object or None
+    """
     try:
-        library = Library.objects.get(name=library_name)
-        return library.librarian
-    except Library.DoesNotExist:
+        return Librarian.objects.get(library__name=library_name)
+    except Librarian.DoesNotExist:
         return None
