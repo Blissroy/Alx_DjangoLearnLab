a5818a06677d05d7787b1058b5b39021e2b1ae0b
Blissroy <fredricktieku2@gmail.com>
2025-07-20 11:44:01 +0000

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -8,8 +8,12 @@ def list_books(request):
 from django.views.generic.detail import DetailView
 from .models import Library
 
-# Class-Based View
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
-    context_object_name = 'library'
+    template_name = 'relationship_app/library_detail.html'
+
+    # Override context to include books in the library
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['books'] = self.object.book_set.all()
+        return context
