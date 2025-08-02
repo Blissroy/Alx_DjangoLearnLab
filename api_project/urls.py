297b18aa314f37beb66b191c59977fa87f440371
Blissroy <fredricktieku2@gmail.com>
2025-08-02 16:04:17 +0000

    Update urls.py

--- api_project/urls.py
+++ api_project/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include  # Make sure to import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # This is the critical line that's missing
+    path('api/', include["api.urls"]),  # This is the critical line that's missing
 ]
