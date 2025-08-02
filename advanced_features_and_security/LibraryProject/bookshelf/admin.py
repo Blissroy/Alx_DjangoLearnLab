417192b03ff8e0689f708b50a6cabeb1ef43f34a
Blissroy <fredricktieku2@gmail.com>
2025-08-02 10:32:29 +0000

    Update admin.py

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,21 +1,18 @@
-from django.contrib import admin
-from .models import Book
 
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year', 'created_at')
-    list_filter = ('author', 'publication_year')
-    search_fields = ('title', 'author')
-    ordering = ('-created_at',)
-    date_hierarchy = 'created_at'
-    
-    fieldsets = (
-        ('Basic Information', {
-            'fields': ('title', 'author')
-        }),
-        ('Publication Details', {
-            'fields': ('publication_year',),
-            'classes': ('collapse',)
-        }),
+# users/admin.py
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
     )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
 
