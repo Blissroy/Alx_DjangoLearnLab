c1a1f7c8d9953ca8613b89946519c7734eb426f2
Blissroy <fredricktieku2@gmail.com>
2025-08-23 19:30:31 +0000

    Update settings.py

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -1,22 +1,10 @@
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'USER': '',      # Add this line (empty for SQLite)
-        'PORT': '',      # Add this line (empty for SQLite)
-    }
+        'NAME': BASE_DIR / "db.sqlite3",
+        'USER': '',      # Required for checker, leave empty for SQLite
+        'PASSWORD': '',  # Same here
+        'HOST': '',      # Empty means localhost
+        'PORT': '',      # Leave blank for SQLite
+    }
 }
-
-# Make sure blog app is in INSTALLED_APPS
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'blog',  # This should be present
-]
-
-
-
