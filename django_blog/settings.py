85b80a67233b4890f1fe4fb490c82b7d6f9272f2
Blissroy <fredricktieku2@gmail.com>
2025-08-23 20:30:07 +0000

    Update settings.py

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -1,10 +1,10 @@
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
-        'USER': '',       # Required key (even if unused in SQLite)
+        "ENGINE": 'django.db.backends.sqlite3',
+        "NAME": BASE_DIR / "db.sqlite3",
+        "USER": '',       # Required key (even if unused in SQLite)
         'PASSWORD': '',   # Required key
-        'HOST': 'localhost',  # Explicit for clarity
-        'PORT': '',       # Required key
+        "HOST": 'localhost',  # Explicit for clarity
+        "PORT": '',       # Required key
     }
 }
