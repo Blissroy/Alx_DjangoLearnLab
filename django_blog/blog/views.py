cc6a29f5e0784c6c13e63eb52dda4234920a6a29
Blissroy <fredricktieku2@gmail.com>
2025-08-24 14:18:04 +0000

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -65,4 +65,75 @@ def profile_view(request):
         'p_form': p_form
     }
     return render(request, 'blog/profile.html', context)
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib import messages
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post, UserProfile
+from .forms import CustomUserCreationForm, UserUpdateForm, ProfileUpdateForm, PostForm
+from django.urls import reverse_lazy
+
+# Authentication views (keep your existing ones)
+def register_view(request):
+    # ... your existing register view code ...
+
+def login_view(request):
+    # ... your existing login view code ...
+
+@login_required
+def logout_view(request):
+    # ... your existing logout view code ...
+
+@login_required
+def profile_view(request):
+    # ... your existing profile view code ...
+# Blog Post CRUD Views
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = 5
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        messages.success(self.request, 'Your post has been created successfully!')
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+def form_valid(self, form):
+        messages.success(self.request, 'Your post has been updated successfully!')
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('post_list')
+    
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+    
+    def delete(self, request, *args, **kwargs):
+        messages.success(request, 'Your post has been deleted successfully!')
+        return super().delete(request, *args, **kwargs)
+
 
