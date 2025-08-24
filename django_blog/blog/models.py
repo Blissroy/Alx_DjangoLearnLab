a1bc51ce95b3ec7c2247ca539e0040dce681a895
Blissroy <fredricktieku2@gmail.com>
2025-08-24 13:03:31 +0000

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -9,4 +9,23 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
+from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', default='default.jpg')
+    
+    def __str__(self):
+        return f'{self.user.username} Profile'
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+      if self.profile_picture:
+            img = Image.open(self.profile_picture.path)
+            if img.height > 300 or img.width > 300:
+                output_size = (300, 300)
+                img.thumbnail(output_size)
+                img.save(self.profile_picture.path)
