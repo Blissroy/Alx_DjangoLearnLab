5d807de23a28466d4d5c13a01547b5eede028131
Blissroy <fredricktieku2@gmail.com>
2025-08-28 00:41:20 +0000

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -64,5 +64,5 @@ app_name = "blog"
 
 urlpatterns = [
     path("search/", views.search_posts, name="search_posts"),
-    path("tags/<slug:slug>/", views.posts_by_tag, name="posts_by_tag"),
+    path("tags/<slug:tag_slug>/" , PostByTagListView.as_view() , name="posts_by_tag"),
 ]
