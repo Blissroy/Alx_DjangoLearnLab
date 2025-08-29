7aaf8557f5e58d067c006a521d6b6f1bee5b5ca2
Fredrick Tieku Avornyo <fredricktieku2@gmail.com>
2025-08-29 11:51:08 +0000

    Implementing Posts and Comments Functionality

--- social_media_api/accounts/admin.py
+++ social_media_api/accounts/admin.py
@@ -3,10 +3,9 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
 
 
-
-
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
-fieldsets = BaseUserAdmin.fieldsets + (
-('Extra', {'fields': ('bio', 'profile_picture', 'followers')}),
-)
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ('Extra', {'fields': ('bio', 'profile_picture', 'followers')}),
+    )
+
--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,23 +1,19 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.conf import settings
-
-
 
 
 class User(AbstractUser):
-bio = models.TextField(blank=True, null=True)
-profile_picture = models.ImageField(upload_to='profiles/', blank=True, null=True)
-# followers: users who follow this user
-followers = models.ManyToManyField(
-'self',
-symmetrical=False,
-related_name='following',
-blank=True,
-)
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profiles/', blank=True, null=True)
 
+    # followers: users who follow this user
+    followers = models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        related_name='following',
+        blank=True,
+    )
 
-def __str__(self):
-return self.usernamefrom django.db import models
+    def __str__(self):
+        return self.username
 
-# Create your models here.
